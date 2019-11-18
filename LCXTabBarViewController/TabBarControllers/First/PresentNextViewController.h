01e0e34c0fdf63ae49f54812cdf77fe7dc068dd5
lcx <leichunxiang58@163.com>
2019-11-18 09:43:31 +0800

    presentVC with Navigation

---
+++ LCXTabBarViewController/TabBarControllers/First/PresentNextViewController.h
@@ -0,0 +1,17 @@
+//
+//  PresentNextViewController.h
+//  LCXTabBarViewController
+//
+//  Created by lcx on 2019/11/18.
+//  Copyright © 2019 lcx. All rights reserved.
+//
+
+#import "SuperViewController.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PresentNextViewController : SuperViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
